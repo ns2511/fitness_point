8d1654979867c2161df10866afffb30e17c88b22
naresh-chharediya <77925755+naresh-chharediya@users.noreply.github.com>
2021-05-26 19:43:00 +0530

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+console.log("i'm naresh");
+function dropdownshow(){
+    // let dropdownbtn = document.getElementById('dropdown_btn');
+    let dropdown = document.getElementById('dropdown_menu');
+
+    if(dropdown.style.display != 'none'){
+        dropdown.style.display = 'none';
+    }
+    else{
+        dropdown.style.display = 'block';
+    }
+
+
+};
